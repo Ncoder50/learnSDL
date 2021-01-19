b179ed560ff44e913fb58228a10eac8ff01a9453
ncoder50@gmail.com <ncoder50@gmail.com>
2021-01-20 00:50:22 +0100

    add main loop to game

--- pacman.cpp
+++ pacman.cpp
@@ -2,12 +2,21 @@
 #include <SDL2/SDL.h>
 
 using namespace std;
+enum KeyPressSurfaces {
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_TOTAL
+};
 
 const int SCREEN_WIDTH = 3440;
 const int SCREEN_HEIGHT = 1440;
 
 bool init(SDL_Window** window, SDL_Surface** surface);
 bool load_image(SDL_Surface* surface, const char* image_path);
+void main_loop(SDL_Window* window, SDL_Surface* surface);
 void update(SDL_Window* window);
 void close(SDL_Window* window);
 
@@ -20,14 +29,23 @@ int main(int argc, char* args[]) {
     if (!init(&window, &surface))
         cout << "Initialization failed!\n";
 
-    if (!load_image(surface, image_path))
-        cout << "Loading image failed\n";
-
-    update(window);
-    SDL_Delay(2000);
+    main_loop(window, surface);
     close(window);
-
     return 0;
+}
+
+void main_loop(SDL_Window* window, SDL_Surface* surface) {
+    SDL_Event e;
+    while (true) {
+        while(SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT)
+                return;
+            if (e.type == KEY_PRESS_SURFACE_DOWN) {
+                load_image(surface, "images/hello_world.bmp");
+                update(window);
+            }
+        }
+    }
 }
 
 bool init(SDL_Window** window, SDL_Surface** surface) {
